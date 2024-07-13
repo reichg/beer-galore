c44d1cb82860a352269e66dca0ef8f9eab4d1b1d
Gabe Reichenberger <gabe.reichenberger@gmail.com>
2024-07-12 17:19:28 -0700

    moved project to new maven monorepo and updated some of the application properties

---
+++ src/main/java/com/evolutionaryeyes/beer_galore_monorepo/BeerGaloreMonorepoApplication.java
@@ -0,0 +1,13 @@
+package com.evolutionaryeyes.beer_galore_monorepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerGaloreMonorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerGaloreMonorepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evolutionaryeyes/beer_galore_monorepo/BeerGaloreMonorepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evolutionaryeyes.beer_galore_monorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerGaloreMonorepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
